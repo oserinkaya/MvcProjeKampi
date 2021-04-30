e9edcaeb86aa951646cf3571c1625412b67c4c7c
oserinkaya <oktayse@yahoo.com>
2021-04-30 12:30:51 +0300

    Ders_26
    MVC Proje Kampı 26ncı ders kapsamında yapılan ekleme hataları çalışmaları

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -19,16 +19,16 @@ namespace BusinessLayer.Concrete
 
         public void CategoryAddBL(Category p)
         {
-            repo.Insert(p);
+            //repo.Insert(p);
 
-            //if (p.CategoryName=="" || p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length >= 51)
-            //{
-            //    //HATA MESAJI
-            //}
-            //else
-            //{
-            //    repo.Insert(p);
-            //}
+            if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51)
+            {
+                //HATA MESAJI
+            }
+            else
+            {
+                repo.Insert(p);
+            }
         }
     }
 }
