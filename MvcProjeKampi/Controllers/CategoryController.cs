19c01fb089bcdcf047a51d56ae78706bcb398216
oserinkaya <oktayse@yahoo.com>
2021-05-04 16:10:01 +0300

    Ders_32
    MVC Proje Kampı 32nci ders kapsamında "ValidationMessageFor" kullanımı ve mesajların UI katmanında görüntülenmesi çalışmaları.

--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -42,7 +42,7 @@ namespace MvcProjeKampi.Controllers
             ValidationResult results = categoryValidator.Validate(p);
             if (results.IsValid)
             {
-                cm.CategoryAddBL(p);
+                cm.CategoryAdd(p);
                 return RedirectToAction("GetCategoryList");
             }
             else
