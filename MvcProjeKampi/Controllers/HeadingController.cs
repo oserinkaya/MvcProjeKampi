78f985bea9b14f1b3b7040d909baafb5a72102c3
oserinkaya <oktayse@yahoo.com>
2021-05-19 20:07:37 +0300

    Ders_47
    MVC Proje Kampı 47 nci ders kapsamında yapılan başlık işlemlerinde dropdownlist ve ilişki kullanımı çalışmaları

--- MvcProjeKampi/Controllers/HeadingController.cs
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +13,34 @@ namespace MvcProjeKampi.Controllers
     {
         // GET: Heading
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
 
         public ActionResult Index()
         {
             var headingvalues = hm.GetList();
 
             return View(headingvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.HeadingAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }
