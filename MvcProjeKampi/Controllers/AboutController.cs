3f3d65074d3e20abfbbaa96dd2d1d187023328f1
oserinkaya <oktayse@yahoo.com>
2021-05-29 14:03:18 +0300

    Ders_57
    MVC Proje Kampı 57 nci ders kapsamında yapılan Hakkımda sayfasında popup modal üzerinden veri ekleme çalışmaları

--- MvcProjeKampi/Controllers/AboutController.cs
+++ MvcProjeKampi/Controllers/AboutController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,25 @@ namespace MvcProjeKampi.Controllers
     public class AboutController : Controller
     {
         // GET: About
+        AboutManager abm = new AboutManager(new EfAboutDal());
+
         public ActionResult Index()
         {
+            var aboutvalues = abm.GetList();
+            return View(aboutvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAbout(About p)
+        {
+            abm.AboutAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }
