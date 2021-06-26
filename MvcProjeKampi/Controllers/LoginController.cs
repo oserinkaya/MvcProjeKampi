071546155951683c91a666449e7807c0985956ef
oserinkaya <oktayse@yahoo.com>
2021-06-26 15:51:35 +0300

    Ders_71
    MVC Proje Kampı 71 inci ders kapsamında yapılan authentication ve session çalışmaları

--- MvcProjeKampi/Controllers/LoginController.cs
+++ MvcProjeKampi/Controllers/LoginController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MvcProjeKampi.Controllers
 {
@@ -24,6 +25,8 @@ namespace MvcProjeKampi.Controllers
             var adminuserinfo = c.Admins.FirstOrDefault(x => x.AdminUserName == p.AdminUserName && x.AdminPassword == p.AdminPassword);
             if (adminuserinfo != null)
             {
+                FormsAuthentication.SetAuthCookie(adminuserinfo.AdminUserName, false);
+                Session["AdminUserName"] = adminuserinfo.AdminUserName;
                 return RedirectToAction("Index", "AdminCategory");
             }
             else
