bdb1553d3bca634b0eb0ca5afb0f8510ea13b287
oserinkaya <oktayse@yahoo.com>
2021-07-06 00:44:10 +0300

    Ders_95
    MVC Proje Kampı 95 inci ders kapsamında yapılan verilerin çeşitli formatlarda raporlanması çalışmaları

--- MvcProjeKampi/Controllers/HeadingController.cs
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -23,6 +23,13 @@ namespace MvcProjeKampi.Controllers
             return View(headingvalues);
         }
 
+        public ActionResult HeadingReport()
+        {
+            var headingvalues = hm.GetList();
+
+            return View(headingvalues);
+        }
+
         [HttpGet]
         public ActionResult AddHeading()
         {
